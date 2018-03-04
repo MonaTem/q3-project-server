355260f273f33935f578c05dcff2bc1d17242ad6
Mona Temchin <monatemchin@gmail.com>
2018-03-04 16:52:33 -0500

    Reinstalled some npm pkgs, deleted www page

--- routes/cards.js
+++ routes/cards.js
@@ -1,25 +1,18 @@
-var express = require('express');
-var router = express.Router();
-
-const bodyParser = require('body-parser');
-const knex = require('./db')
-
-// Middleware
-
-app.use(morgan('combined'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const app = require('express').Router();
 
 // Routes
+// console.log("we are in the cards route");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+app.get('/', function(req, res, next) {
   response.send('The slash route is working, so you better start working.');
 });
 
-router.get('/Cards', (request, response) => {
+app.get('/Cards', (request, response) => {
   knex('cards')
   .then( rows => response.json(cards));
 });
 
-module.exports = router;
+console.log('at end of cards route');
+
+module.exports = app;
--- server.js
+++ server.js
@@ -4,8 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var dotenv = require('dotenv').config();
 
-var cards = require('./routes/cards');
+const cards = require('./routes/cards');
 // var users = require('./routes/users');
 
 var app = express();
@@ -13,16 +14,26 @@ const PORT = process.env.PORT || 6000;
 
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+// Setup Middleware
+
+app.use(express.static('public'))
+app.use(logger('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+
+
 
 app.use('/', cards);
 // app.use('/users', users);
@@ -46,5 +57,10 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(PORT, () => {
+  console.log('Server listening on ', PORT);
+});
+
 
 module.exports = app;
